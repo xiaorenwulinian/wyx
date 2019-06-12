e20572e29c752d7810d5016d8d089bc1029635aa
lcl123456 <13586764835@163.com>
2019-06-12 08:23:40 +0800

    新增导出工具类

---
+++ application/common/controller/ExcelUnit.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019\6\12 0012
+ * Time: 8:16
+ */
+namespace app\common\controller;
+
+use think\Controller;
+
+class ExcelUnit extends Controller
+{
+    /**
+     * excel 导出
+     */
+    public function excelExport()
+    {
+
+    }
+}
